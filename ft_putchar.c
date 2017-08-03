5699af6ceea2af69449d78af34fd267c447a2e42
Rahisul Haque <rhaque@e1z3r6p2.42.us.org>
2017-08-02 18:25:00 -0700

    Revisiting libft

--- ft_putchar.c
+++ ft_putchar.c
@@ -6,7 +6,7 @@
 /*   By: rhaque <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/02 17:43:23 by rhaque            #+#    #+#             */
-/*   Updated: 2017/08/02 17:45:53 by rhaque           ###   ########.fr       */
+/*   Updated: 2017/08/02 18:18:39 by rhaque           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,22 @@ void	ft_putchar(char ch)
 	write(1, &ch, 1);
 }
 
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while(str[i] != 0)
+	{
+		ft_putchar(str[i++]);
+	}
+}
+
+
 int		main(void)
 {
 	ft_putchar('z');
+	ft_putchar('\n');
+	ft_putstr("Hello World\n");
 	return (0);
 }
